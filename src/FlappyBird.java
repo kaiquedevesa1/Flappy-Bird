04785d2a3ec5872b5757c9d61fb41e9e12de6588
kaiquedevesa1 <kaikedaeves@hotmail.com>
2025-04-08 20:52:49 -0300

    add pipes

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -32,13 +32,43 @@ public class FlappyBird extends JPanel{
         
     }
 
+    //Canos
+    int PipeX = LarguraBorda;
+    int PipeY = 0;
+    int PipeWidth = 64;
+    int PipeHeight = 512;
 
+    class Pipe {
+        int x = PipeX;
+        int y = PipeY;
+        int width = PipeWidth;
+        int height = PipeHeight;
+        Image img;
+        boolean  Passed = false;
+
+        Pipe(Image img){
+            this.img = img;
+        }
+    }
+
+
+    //Logica do jogo
 
     Bird bird;
+    int VelocityX = -4;
+    int VelocityY = 0;
+    int gravity = 1;
+
+    ArrayList<Pipe> pipes;
+
+    Timer gameLoop;
+    Timer placePipesTimer;
+
 
     FlappyBird(){
         setPreferredSize(new Dimension(LarguraBorda, AlturaBorda));
-       
+        setFocusable(true);
+        addKeyListener(this);
 
         backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
         birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
@@ -48,6 +78,17 @@ public class FlappyBird extends JPanel{
 
 
         bird = new Bird(birdImage);
+        pipes = new ArrayList<Pipe>();
+        placePipesTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        } );
+
+        placePipesTimer.start();
+        gameLoop = new Timer(1000/60, null);
+        gameLoop.start();
     }
     
 
@@ -57,10 +98,49 @@ public class FlappyBird extends JPanel{
         draw(g);
     }
 
+    public void placePipes() {
+        Pipe topPipe = new Pipe(topPipeImage);
+        pipes.add(topPipe);
+    } 
+
+
     public void draw (Graphics g) {
 
         g.drawImage(backgroundImage, 0, 0, LarguraBorda, AlturaBorda,  null);
 
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+
+        for (int i = 0; i < pipes.size(); i++){
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+
+        }
+    }
+
+    public void move(){
+        VelocityY += gravity;
+        bird.y += VelocityY;
+        bird.y = Math.max(bird.y, 0);
+
+        for (int i = 0; i < pipes.size(); i++){
+            Pipe pipe = pipes.get(i);
+            pipe.x += VelocityX;
+        }
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+
+
+
+    @Override
+    public void keyPressed(KeyEvent e){
+        if(e.getKeyCode() == KeyEvent.VK_SPACE){
+            VelocityY = -6;
+        }
     }
 }
